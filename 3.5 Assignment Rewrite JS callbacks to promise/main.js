a894db475304871cd06f4fefe3d462c8f454aeae
Georgios Feidakis <60877672+geofeid@users.noreply.github.com>
2022-04-19 18:13:41 +0300

    Add files via upload

---
+++ 3.5 Assignment Rewrite JS callbacks to promise/main.js
@@ -0,0 +1,28 @@
+const callMe = (firstName, callback) => {
+    setTimeout(() => {
+      if (!firstName) return callback(new Error('no first name passed in!'))
+  
+      const fullName = `${firstName} Smith`
+  
+      return callback(fullName)
+    }, 1000)
+  }
+  
+  callMe('Ben', console.log)
+  callMe(null, console.log)
+  
+
+const c2promise = firstName => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (!firstName) reject(new Error('no firstname'))
+  
+        const fullName = `${firstName} Bruh`  
+  
+        resolve(fullName)
+      }, 1000)
+    })
+  }
+  
+  c2promise('Ben').then(console.log)
+  c2promise().catch(console.log)
